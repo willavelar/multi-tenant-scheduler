817ac365f9159eb1799b3c1222086bdec33f4b9f
William Avelar <avelar.developer@gmail.com>
2026-04-22 20:52:41 -0300

    feat(api): expose GET/PATCH /tenants/me for tenant admins

--- packages/api/src/tenants/tenants.module.ts
+++ packages/api/src/tenants/tenants.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TenantsService } from './tenants.service';
+import { TenantsController } from './tenants.controller';
 
 @Module({
+  controllers: [TenantsController],
   providers: [TenantsService],
   exports: [TenantsService],
 })
