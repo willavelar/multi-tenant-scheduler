54d81d2b61dc91a3a666a10be0fd08f7e585613f
William Avelar <avelar.developer@gmail.com>
2026-05-01 15:32:36 -0300

    feat(api): install bullmq and scaffold email-queue module

---
+++ packages/api/src/email-queue/email-queue.processor.ts
@@ -0,0 +1,18 @@
+// packages/api/src/email-queue/email-queue.processor.ts
+import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Job } from 'bullmq';
+import { EmailService } from '../email/email.service';
+import { EMAIL_QUEUE, InviteJobData } from './email-queue.producer';
+
+@Processor(EMAIL_QUEUE)
+export class EmailQueueProcessor extends WorkerHost {
+  constructor(private readonly emailService: EmailService) {
+    super();
+  }
+
+  async process(job: Job<InviteJobData>): Promise<void> {
+    if (job.name === 'send-invite') {
+      await this.emailService.sendInvite(job.data.to, job.data.inviteUrl);
+    }
+  }
+}
