bd6a83b5221b6ed259e79546c656bee20ab90303
William Avelar <avelar.developer@gmail.com>
2026-06-06 14:02:28 -0300

    feat(web): mount SupportWidget on authenticated tenant pages

--- packages/web/src/app/(tenant)/(app)/layout.tsx
+++ packages/web/src/app/(tenant)/(app)/layout.tsx
@@ -1,12 +1,14 @@
 import { AppShell } from '@/components/AppShell'
 import { UserPreferencesProvider } from '@/providers/UserPreferencesProvider'
 import { TenantSettingsProvider } from '@/providers/TenantSettingsProvider'
+import { SupportWidget } from '@/components/SupportWidget'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <TenantSettingsProvider>
       <UserPreferencesProvider>
         <AppShell>{children}</AppShell>
+        <SupportWidget />
       </UserPreferencesProvider>
     </TenantSettingsProvider>
   )
